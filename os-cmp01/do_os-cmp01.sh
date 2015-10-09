83d09e6d213efb279b986cead9a4f5d9c077908a
Patrice Laberge <Patrice.Laberge@bell.ca>
2015-10-08 22:15:10 -0400

    first commit

---
+++ os-cmp01/do_os-cmp01.sh
@@ -0,0 +1,25 @@
+export hn=cmp01
+
+export IMAGES=/var/lib/libvirt/images
+export CONFIG_DRIVE=./CONFIG-DRIVE
+export ISOs=./ISOs
+
+mkisofs -R -V config-2 -o $ISOs/drive-ubuntu.iso $CONFIG_DRIVE
+
+qemu-img create -f qcow2 -b $IMAGES/trusty-server-cloudimg-amd64-disk1.img $IMAGES/$hn.qcow2 50G
+
+virt-install \
+--name "$hn" \
+--virt-type kvm \
+--vcpus 8 \
+--ram 8192 \
+--os-type=linux \
+--os-variant=ubuntutrusty \
+--disk path=$IMAGES/$hn.qcow2 \
+--disk path=$ISOs/drive-ubuntu.iso \
+--network bridge=br0  \
+--network bridge=br1  \
+--graphics vnc,listen=0.0.0.0 \
+--noautoconsole \
+--import
+
