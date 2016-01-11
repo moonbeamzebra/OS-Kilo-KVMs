f2cc3f9fca4646db7be71f43caa5a6199509cc4d
lab <lab@mvkvm01.lab.local>
2016-01-10 19:46:01 -0500

    add .26, .27, .28

---
+++ net27/do_net27.sh
@@ -0,0 +1,26 @@
+export hn=net27
+
+export IMAGES=/var/lib/libvirt/images
+export CONFIG_DRIVE=./CONFIG-DRIVE
+export ISOs=./ISOs
+
+mkisofs -R -V config-2 -o $ISOs/drive-ubuntu.iso $CONFIG_DRIVE
+
+qemu-img create -f qcow2 -b $IMAGES/trusty-server-cloudimg-amd64-disk1.img $IMAGES/$hn.qcow2 50G
+
+virt-install \
+--name "$hn" \
+--virt-type kvm \
+--vcpus 4 \
+--ram 8192 \
+--os-type=linux \
+--os-variant=ubuntutrusty \
+--disk path=$IMAGES/$hn.qcow2 \
+--disk path=$ISOs/drive-ubuntu.iso \
+--network bridge=br0  \
+--network bridge=br1  \
+--network bridge=br1  \
+--graphics vnc,listen=0.0.0.0 \
+--noautoconsole \
+--import
+
